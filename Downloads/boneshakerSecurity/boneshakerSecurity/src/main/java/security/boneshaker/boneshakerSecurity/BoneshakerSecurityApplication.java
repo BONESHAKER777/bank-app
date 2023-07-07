cb3fce1de99c17dc20035209ad84494d2e51f00a
samuelshola777 <samuelshola14@gmail.com>
2023-07-07 16:10:51 +0100

    the start

---
+++ Downloads/boneshakerSecurity/boneshakerSecurity/src/main/java/security/boneshaker/boneshakerSecurity/BoneshakerSecurityApplication.java
@@ -0,0 +1,13 @@
+package security.boneshaker.boneshakerSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoneshakerSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoneshakerSecurityApplication.class, args);
+	}
+
+}
---
+++ Downloads/boneshakerSecurity/boneshakerSecurity/src/test/java/security/boneshaker/boneshakerSecurity/BoneshakerSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package security.boneshaker.boneshakerSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoneshakerSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
