ae16e7de1102dc99ac0990f3757bc922fe77c8a0
samuelshola777 <samuelshola14@gmail.com>
2023-07-07 16:14:48 +0100

    the start

--- Downloads/boneshakerSecurity/boneshakerSecurity/src/main/java/security/boneshaker/boneshakerSecurity/User.java
+++ Downloads/boneshakerSecurity/boneshakerSecurity/src/main/java/security/boneshaker/boneshakerSecurity/User.java
@@ -5,7 +5,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 
-public class User extends UserDetails {
+public class User implements UserDetails {
+    private long id;
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return null;
